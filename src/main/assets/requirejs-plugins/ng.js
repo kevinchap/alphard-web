fb623c63747ea6590989160a6c0ec918723698c5
Frédéric Nowak <frederic.nowak@gmail.com>
2014-10-19 22:11:21 +0200

    Light refactor

--- src/main/assets/requirejs-plugins/ng.js
+++ src/main/assets/requirejs-plugins/ng.js
@@ -1,6 +1,8 @@
 /*global: window, define */
-define(function () {
+define(['module'], function (plugin) {
     'use strict';
+
+    var angular = plugin.config().angular || 'angular';
 
     function _isAngularModule(o) {
         return (typeof o === 'object') &&
@@ -20,35 +22,37 @@ define(function () {
 
     return {
         load: function load(name, req, onLoad, config) {
-            var angular = config.angular || 'angular';
             req([angular, name], function (angular, module) {
                 if (_isAngularModule(module)) {
                     onLoad(module);
                     return;
                 }
-                var dependencies = module.dependencies || [];
-                var factory = module.factory || (function () {
+                var deps = module.deps || [];
+                var init = module.init || (function () {
                     console.warn('no factory defined for ng module ' + name + '!');
                     return function () {
                     };
                 }());
                 var bootstrap = module.bootstrap || false;
 
-                req(dependencies, function () {
+                req(deps, function () {
                     var ngModule;
                     var resolvedDependencies = [];
                     var angularDependencies = [];
 
                     for (var i = 0, l = arguments.length; i < l; i++) {
                         var resolvedDependency = arguments[i];
-                        resolvedDependencies.push(resolvedDependency);
                         if (_isAngularModule(resolvedDependency)) {
                             angularDependencies.push(resolvedDependency.name);
+                        } else if (deps[i] == 'module') {
+                            resolvedDependencies.push(config)
+                        } else {
+                            resolvedDependencies.push(resolvedDependency);
                         }
                     }
 
                     ngModule = angular.module(name, angularDependencies);
-                    ngModule = factory.apply(this, [ ngModule ].concat(resolvedDependencies)) || ngModule;
+                    ngModule = init.apply(this, [ ngModule ].concat(resolvedDependencies)) || ngModule;
                     onLoad(ngModule);
                     if (bootstrap) {
                         angular.bootstrap(document, [ ngModule.name ]);
