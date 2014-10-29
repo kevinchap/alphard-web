434d06e9c250438da47832626ced4901f47d6491
Julien Polo <julien.polo@sportaculous.com>
2014-10-29 14:59:49 +0100

    Style fixes

--- src/main/assets/angular-ui-spinner/spinner.js
+++ src/main/assets/angular-ui-spinner/spinner.js
@@ -19,9 +19,29 @@ function (
     .module(module.id, [])
     .directive("spinner", function spinner() {
       return {
+        compile: function ($element, attrs) {
+          return function link($scope, $element, attrs) {
+            $scope.size = $scope.size || "md";
+
+            $scope.getClass = function () {
+              var size = $scope.size;
+              return {
+                "spinner": true,
+                "spinner--default": true,
+                "spinner--xs": size === 'xs',
+                "spinner--sm": size === 'sm',
+                "spinner--md": size === 'md',
+                "spinner--lg": size === 'lg'
+              };
+            };
+          };
+        },
         restrict: 'E',
         replace: true,
-        template: spinnerHTML
+        template: spinnerHTML,
+        scope: {
+          size: '@'
+        }
       };
     });
 });
