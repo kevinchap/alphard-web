aa035133e86d64a6a951d551d3fa8700b5f370e0
Frédéric Nowak <frederic.nowak@gmail.com>
2014-11-25 17:47:13 +0100

    no message

--- project/Build.scala
+++ project/Build.scala
@@ -9,26 +9,8 @@ import com.typesafe.sbt.web._
 import sbt.Keys._
 import sbt._
 
-object Build extends Build {
-
-  val lessSettings: Seq[Setting[_]] = Seq(
-    includeFilter in LessKeys.less := "*.less",
-    excludeFilter in LessKeys.less := "_*.less"
-  )
-
-  val mochaSettings: Seq[Setting[_]] = Seq(
-    MochaKeys.requires ++= Seq("_mocha.conf")
-  )
-
-  val assetsSettings = lessSettings
-
-  val buildSettings = mochaSettings ++ Seq(
-    organization := "com.byteground",
-    scalaVersion := "2.10.4",
-    crossPaths := false,
-    importDirectly := true
-  )
-
+object Build
+  extends Build {
   lazy val root =
     bytegroundProject("web-util")
       .enablePlugins(
@@ -36,22 +18,33 @@ object Build extends Build {
         SbtLess,
         SbtWeb
       ).settings(
-        buildSettings ++
-          Seq(
-            libraryDependencies ++= Seq(
-              "org.webjars" % "requirejs" % "2.1.14-3",
-              "org.webjars" % "rjs" % "2.1.11-1-trireme" % "test",
-              "org.webjars" % "angularjs" % "1.3.0",
-              "org.webjars" % "angular-translate" % "2.4.2",
-              "org.webjars" % "angular-ui-router" % "0.2.11",
-              "org.webjars" % "angular-ui-bootstrap" % "0.11.2",
-              "org.webjars" % "q" % "1.0.1",
-              "org.webjars" % "bootstrap" % "3.2.0",
-              "org.webjars" % "font-awesome" % "4.2.0"
-            )
-          ) ++
-          inConfig(Assets)(assetsSettings) ++
-          inConfig(TestAssets)(assetsSettings)
+        organization := "com.byteground",
+        scalaVersion := "2.10.4",
+        crossPaths := false,
+        importDirectly := true,
+        libraryDependencies ++= Seq(
+          "org.webjars" % "requirejs" % "2.1.14-3",
+          "org.webjars" % "rjs" % "2.1.11-1-trireme" % "test",
+          "org.webjars" % "angularjs" % "1.3.0",
+          "org.webjars" % "angular-translate" % "2.4.2",
+          "org.webjars" % "angular-ui-router" % "0.2.11",
+          "org.webjars" % "angular-ui-bootstrap" % "0.11.2",
+          "org.webjars" % "q" % "1.0.1",
+          "org.webjars" % "bootstrap" % "3.2.0",
+          "org.webjars" % "font-awesome" % "4.2.0"
+        ),
+
+        // Mocha
+        MochaKeys.requires ++= Seq("_mocha.conf")
+      ).settings(
+        inConfig(Assets)(unscopedSettings) ++
+          inConfig(TestAssets)(unscopedSettings)
           : _*
       )
+
+  private val unscopedSettings = Seq(
+    // Less
+    includeFilter in LessKeys.less := "*.less",
+    excludeFilter in LessKeys.less := "_*.less"
+  )
 }
