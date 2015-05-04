1a8d90ab8a0ce4c7dd1d0c729959dfbbc3872973
Julien Polo <julien.polo@sportaculous.com>
2015-05-04 16:48:57 +0200

    bootstrap upgrade

--- project/Build.scala
+++ project/Build.scala
@@ -26,7 +26,7 @@ object Build
           "org.webjars" % "requirejs" % "2.1.15",
           "org.webjars" % "angularjs" % "1.3.10",
           "org.webjars" % "q" % "1.0.1",
-          "org.webjars" % "bootstrap" % "3.3.1",
+          "org.webjars" % "bootstrap" % "3.3.4",
           "org.webjars" % "flag-icon-css" % "0.6.3"
         )
       ).settings(
