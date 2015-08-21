84c294cb8b0e0bf4ce50c8f4a7d692170f74103d
Julien Polo <julien.polo@sportaculous.com>
2015-08-21 16:03:30 +0200

    angular flags bugfixes

--- src/main/assets/angular-flags/flags.js
+++ src/main/assets/angular-flags/flags.js
@@ -5,6 +5,100 @@ define(["module", "angular"], function (module, angular) {
   var config = (module.config && module.config()) || {};
   var DEBUG = config.debug || false;
   var CSS = config.css || "css!flag-icon-css/css/flag-icon";
+  var isoAlpha3To2 = {
+    "AND": "AD",
+    "ARE": "AE",
+    "ATG": "AG",
+    "ARM": "AM",
+    "AGO": "AO",
+    "ATA": "AQ",
+    "AUT": "AT",
+    "ABW": "AW",
+    "BIH": "BA",
+    "BRB": "BB",
+    "BGD": "BD",
+    "BDI": "BI",
+    "BEN": "BJ",
+    "BRN": "BN",
+    "BES": "BQ",
+    "BHS": "BS",
+    "BLR": "BY",
+    "BLZ": "BZ",
+    "COD": "CD",
+    "CAF": "CF",
+    "COG": "CG",
+    "COK": "CK",
+    "CHL": "CL",
+    "CHN": "CN",
+    "CPV": "CV",
+    "CUW": "CW",
+    "DNK": "DK",
+    "EST": "EE",
+    "ESH": "EH",
+    "FLK": "FK",
+    "FSM": "FM",
+    "FRO": "FO",
+    "GRD": "GD",
+    "GUF": "GF",
+    "GRL": "GL",
+    "GIN": "GN",
+    "GLP": "GP",
+    "GNQ": "GQ",
+    "SGS": "GS",
+    "GNB": "GW",
+    "GUY": "GY",
+    "IRL": "IE",
+    "ISR": "IL",
+    "IRQ": "IQ",
+    "JAM": "JM",
+    "COM": "KM",
+    "PRK": "KP",
+    "KOR": "KR",
+    "CYM": "KY",
+    "KAZ": "KZ",
+    "LBR": "LR",
+    "LBY": "LY",
+    "MNE": "ME",
+    "MAF": "MF",
+    "MDG": "MG",
+    "MAC": "MO",
+    "MNP": "MP",
+    "MTQ": "MQ",
+    "MLT": "MT",
+    "MDV": "MV",
+    "MEX": "MX",
+    "MOZ": "MZ",
+    "NIU": "NU",
+    "PYF": "PF",
+    "PNG": "PG",
+    "PAK": "PK",
+    "POL": "PL",
+    "SPM": "PM",
+    "PCN": "PN",
+    "PRT": "PT",
+    "PLW": "PW",
+    "PRY": "PY",
+    "SRB": "RS",
+    "SLB": "SB",
+    "SYC": "SC",
+    "SWE": "SE",
+    "SVN": "SI",
+    "SVK": "SK",
+    "SEN": "SN",
+    "SUR": "SR",
+    "SLV": "SV",
+    "SWZ": "SZ",
+    "TCD": "TD",
+    "ATF": "TF",
+    "TKM": "TM",
+    "TUN": "TN",
+    "TUR": "TR",
+    "TUV": "TV",
+    "UKR": "UA",
+    "URY": "UY",
+    "WLF": "WF",
+    "MYT": "YT"
+  };
 
   //Util
   function str(o) {
@@ -26,28 +120,34 @@ define(["module", "angular"], function (module, angular) {
       console.debug.apply(console, args);
     }
   }
+
+  function isoAlpha2(s) {
+    s = str(s).toUpperCase();
+    var returnValue = s;
+    if (isoAlpha3To2.hasOwnProperty(s)) {
+      returnValue = isoAlpha3To2[s];
+    } else {
+      returnValue = s.slice(0, 2);//Take only first two letters
+    }
+    return returnValue.toLowerCase();
+  }
+
   debug("config", config);
 
   //angular module
   return angular
     .module(module.id, [])
 
-    /**
-     * Flag directive
-     *
-     * Usage:
-     *
-     * <flag country="fr" [squared="true|false"]></flag>
-     */
+  /**
+   * Flag directive
+   *
+   * Usage:
+   *
+   * <flag country="fr" [squared="true|false"]></flag>
+   */
     .directive("flag", [function () {
       var $$class = "flag-icon";
       var $m = bem($$class, "-");
-
-      function _countryCode(s) {
-        var returnValue = str(s);
-        returnValue = s.slice(0, 2).toLowerCase();//Take only first two letters
-        return returnValue;
-      }
 
       return {
         restrict: "EA",
@@ -60,14 +160,16 @@ define(["module", "angular"], function (module, angular) {
           if (CSS) {
             require([CSS]);
           }
-          return function link($scope, $element, $attrs) {
+          return function link($scope, $element) {
 
-            $scope.$watch(function () { $element.addClass($$class); });
+            $scope.$watch(function () {
+              $element.addClass($$class);
+            });
             $scope.$watch("country", function (country, countryOld) {
               debug($element[0], "country=", country);
               $element
-                .removeClass($m(_countryCode(countryOld)))
-                .addClass($m(_countryCode(country)));
+                .removeClass($m(isoAlpha2(countryOld)))
+                .addClass($m(isoAlpha2(country)));
             });
             $scope.$watch("squared", function (squared) {
               debug($element[0], "squared=", squared);
