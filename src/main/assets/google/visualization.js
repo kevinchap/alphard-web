8ac57644c728fd4842456b2b57d2e58458cd1025
Julien Polo <julien.polo@sportaculous.com>
2015-10-13 14:50:30 +0200

    google map & visualization

--- src/main/assets/google/visualization.js
+++ src/main/assets/google/visualization.js
@@ -1,3 +1,13 @@
-define(["goog!visualization,1,packages:[corechart,geochart]"], function () {
+define([
+  "goog!" + (function () {
+    var moduleConfig = requirejs.s.contexts._.config.config["google/visualization"] || {};
+
+    return [
+      "visualization",
+      moduleConfig.version || "1.1",
+      moduleConfig.settings || 'packages:[corechart,geochart,table]'
+    ].join(",");
+  }())
+], function () {
   return google.visualization;
 });
