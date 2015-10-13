e61c24e3aa8e22d4380eaa2001a6be595d893e0c
Julien Polo <julien.polo@sportaculous.com>
2015-10-13 12:01:36 +0200

    depend added

---
+++ src/main/assets/requirejs-plugins/depend.js
@@ -0,0 +1,41 @@
+define(function () {
+
+  /**
+   * depend module
+   *
+   * Usage:
+   *   depend!bar[jquery,lib/foo]
+   */
+  var depend;
+  (function (depend) {
+    var reParts = /^(.*)\[([^\]]*)\]$/;
+
+    function parseName(s) {
+      var parts = reParts.exec(s);
+      var parts_2 = parts[2].split(',');
+      var deps = [];
+      for (var i = 0, l = parts_2.length; i < l; i++) {
+        deps.push(parts_2[i].trim());
+      }
+      return {
+        module: parts[1],
+        dependencies: deps
+      };
+    }
+
+    function load(name, parentRequire, onLoad, config) {
+      var parsed = parseName(name);
+
+      parentRequire(parsed.dependencies, function () {
+        parentRequire([parsed.module], function (mod) {
+          onLoad(mod);
+        });
+      });
+    }
+
+    depend.load = load;
+
+  }(depend || (depend = {})));
+
+  return depend;
+});
