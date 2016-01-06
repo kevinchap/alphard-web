427f4153d33bb63b8c7b479221a68ea65b193e33
Julien Polo <julien.polo@sportaculous.com>
2016-01-06 18:56:38 +0100

    bugfix

--- src/main/assets/angular-translate-loader/loader.js
+++ src/main/assets/angular-translate-loader/loader.js
@@ -5,33 +5,17 @@ define(["module", "angular", "angular-translate-loader-partial"], function (modu
     .module(module.id, [ngTranslateLoader.name || ngTranslateLoader])
 
     .config(["$linkProvider", function ($linkProvider) {
-      $linkProvider.set("ng-stylesheet", "$linkStylesheet");
-    }])
-    .factory("$linkStylesheet", [function () {
+      $linkProvider
+        .rel("ng-stylesheet", [function () {
 
-      return function loader($scope, $element, $attrs) {
-        var href = $element.attr("href");
-        if (href) {
-          var comment = ' require(css!' + href + ')';
-          require(["css!" + href]);
-          $element.replaceWith(document.createComment(comment));
-        }
-      };
-    }])
-
-    .config(["$linkProvider", function ($linkProvider) {
-      $linkProvider.set("ng-translate", "$linkTranslate");
-    }])
-    .factory("$linkTranslate", ["$translate", "$translatePartialLoader", function ($translate, $translatePartialLoader) {
-
-      return function loader($scope, $element, $attrs) {
-        var href = $element.attr("href");
-        if (href) {
-          var comment = ' $translatePartialLoader.addPart(' + href + ')';
-          $translatePartialLoader.addPart(href);
-          $translate.refresh();
-          $element.replaceWith(document.createComment(comment));
-        }
-      };
+          return function loader($scope, $element, $attrs) {
+            var href = $element.attr("href");
+            if (href) {
+              var comment = ' require(css!' + href + ')';
+              require(["css!" + href]);
+              $element.replaceWith(document.createComment(comment));
+            }
+          };
+        }]);
     }]);
 });
