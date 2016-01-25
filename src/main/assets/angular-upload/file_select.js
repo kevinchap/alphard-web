cbeac1cb1dac8b1f0f83cc2659d1a5fe5026f73e
Julien Polo <julien.polo@sportaculous.com>
2016-01-25 18:15:35 +0100

    Bugfix for angular material (ng-file-select + md-button) and code cleaning

--- src/main/assets/angular-upload/file_select.js
+++ src/main/assets/angular-upload/file_select.js
@@ -9,6 +9,7 @@ define(["module", "angular"], function (module, angular) {
     .directive("ngFileSelect", NgFileSelect);
 
 
+
   /**
    * ngFileSelect directive
    *
@@ -22,10 +23,37 @@ define(["module", "angular"], function (module, angular) {
    *
    *  </button>
    */
-  NgFileSelect.$inject = ["$compile", "$log", "$parse"];
-  function NgFileSelect($compile, $log, $parse) {
+  NgFileSelect.$inject = ["$compile", "$document", "$log", "$parse"];
+  function NgFileSelect($compile, $document, $log, $parse) {
     var NAME = "ngFileSelect";
     var MULTIPLE = "multiple";
+    var STYLE =
+      '<style type="text/css">' +
+
+        //Styling the parent element
+      '.ng-file-select, [ng-file-select] {' +
+      '  position: relative; ' +
+      '  overflow: hidden; ' +
+      '}\n' +
+
+        //Hide the input overlay element
+      '.ng-file-select__input {' +
+      '  cursor: pointer;' +
+      '  display: block;' +
+      '  position: absolute;' +
+      '  top: 0;' +
+      '  left: 0;' +
+      '  width: 100%;' +
+      '  height: 100%;' +
+      '  opacity: 0;' +
+      '  filter: "~alpha(opacity=0)";' +
+      '  z-index: 99;' +
+      '  outline: 0;' +
+      '}\n' +
+      '.ng-file-select__input::-webkit-file-upload-button {' +
+      '  cursor: pointer;' +
+      '}\n' +
+      '</style>';
 
     function __toArray(o) {
       var i, l;
@@ -48,22 +76,31 @@ define(["module", "angular"], function (module, angular) {
       return returnValue;
     }
 
+    function includeStyle($document, style) {
+      //Small snippet that set an input file element as overlay
+      return angular
+        .element($document)
+        .find("head")
+        .prepend(style);
+    }
+
+    includeStyle($document, STYLE);
+
     return {
+      priority: 5,
       restrict: "A",
       compile: function () {
-
         return function link($scope, $element, $attrs) {
           var $inputElement = $element;
           var $inputElementChild = false;
           var nodeName = $element[0].nodeName;
           var ngFileSelect = $parse($attrs[NAME]);
 
-
           function initialize() {
             //Update DOM
             if (nodeName !== "INPUT") {
               //$inputScope = $scope.$new();
-              $inputElement = $compile('<input type="file" ng-click="$event.stopPropagation();">')($scope);
+              $inputElement = $compile('<input type="file" class="ng-file-select__input" ng-click="$event.stopPropagation();">')($scope);
               $inputElementChild = true;
               $element.append($inputElement);
               $scope.$watch(
