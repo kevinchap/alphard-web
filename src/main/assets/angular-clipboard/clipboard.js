402aea729e459b4533c406c67fafdf49f0d6d4ad
Julien Polo <julien.polo@sportaculous.com>
2016-02-05 15:47:55 +0100

    angular-debounce deprecated

--- src/main/assets/angular-clipboard/clipboard.js
+++ src/main/assets/angular-clipboard/clipboard.js
@@ -13,59 +13,83 @@ define(["module", "angular", "dom/clipboard"], function (module, angular, clipbo
     }
   }
 
-  return angular
+  /**
+   * Angular module
+   */
+  var ngModule = angular
     .module(module.id, [])
+    .provider("$clipboard", ClipboardProvider)
+    .directive("ngClickCopy", NgClickCopy);
 
-    .provider("$clipboard", function () {
-      this.$get = [function () {
-        return clipboard;
-      }];
-    })
+  /**
+   * $clipboard provider
+   *
+   * @constructor
+   */
+  function ClipboardProvider() {
+    this.$get = [function () {
+      return clipboard;
+    }];
+  }
 
-    /**
-     * Usage:
-     *
-     * <button ng-click-copy="expr"
-     *         [ng-copy="fn($text)"]
-     *         [ng-copy-error="fn($error)"]>
-     * </button>
-     */
-    .directive("ngClickCopy", ["$clipboard", "$exceptionHandler", "$parse", function ($clipboard, $exceptionHandler, $parse) {
 
-      return {
-        restrict: 'A',
-        compile: function ($element, $attrs) {
-          var ngClickCopy = $parse($attrs.ngClickCopy, /* interceptorFn */ null, /* expensiveChecks */ true);
-          var ngCopy = $parse($attrs.ngCopy, null, true);
-          var ngCopyError = $attrs.ngCopyError ? $parse($attrs.ngCopyError, null, true) : function (locals) {
-            $exceptionHandler(locals && locals.$error);
-          };
-          return function ngEventHandler($scope, $element) {
-            $element.on('click', function ($event) {
-              debug("Click event", $event);
+  /**
+   * Directive
+   *
+   * @usage
+   *
+   * <button ng-click-copy="expr"
+   *         [ng-copy="fn($text)"]
+   *         [ng-copy-error="fn($error)"]>
+   * </button>
+   */
+  NgClickCopy.$directive = "ngClickCopy";
+  NgClickCopy.$inject = ["$clipboard", "$exceptionHandler", "$parse"];
+  function NgClickCopy($clipboard, $exceptionHandler, $parse) {
+    return {
+      restrict: "A",
+      compile: function ($element, $attrs) {
+        var ngClickCopy = $parse($attrs[NgClickCopy.$directive], /* interceptorFn */ null, /* expensiveChecks */ true);
+        var ngCopy = $parse($attrs.ngCopy, null, true);
+        var ngCopyError = $attrs.ngCopyError ? $parse($attrs.ngCopyError, null, true) : function (locals) {
+          $exceptionHandler(locals && locals.$error);
+        };
+        return function ngEventHandler($scope, $element) {
 
-              var text = ngClickCopy($scope, {  });
-              var callback = function () {
-                ngCopy($scope, { $text: text });
-              };
-              debug(text, "sending to clipboard");
-              try {
-                //copy text in clipboard
-                $clipboard.copyText(text);
-                debug(text, "OK");
-                if ($scope.$root.$$phase) {
-                  $scope.$evalAsync(callback);
-                } else {
-                  $scope.$apply(callback);
-                }
-              } catch (err) {
-                debug(text, "Error:", err);
-                ngCopyError({ $error: err });
+          function onClick($event) {
+            debug("Click event", $event);
+
+            var text = ngClickCopy($scope, {  });
+            var callback = function () {
+              ngCopy($scope, { $text: text });
+            };
+            debug(text, "sending to clipboard");
+            try {
+              //copy text in clipboard
+              $clipboard.copyText(text);
+              debug(text, "OK");
+              if ($scope.$root.$$phase) {
+                $scope.$evalAsync(callback);
+              } else {
+                $scope.$apply(callback);
               }
-            });
-          };
-        }
-      };
-    }]);
+            } catch (err) {
+              debug(text, "Error:", err);
+              ngCopyError({ $error: err });
+            }
+          }
+
+          function onDestroy() {
+            $element.unbind('click', onClick);
+          }
+
+          $element.bind('click', onClick);
+          $scope.$on("$destroy", onDestroy);
+        };
+      }
+    };
+  }
+
+  return ngModule;
 });
 
