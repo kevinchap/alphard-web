f516b91f6c5049ce463d7b825cf9c4446c831f59
Frédéric Nowak <frederic.nowak@gmail.com>
2016-02-29 23:30:48 +0100

    Refactor logger and util.

--- src/main/assets/logger/logger.js
+++ src/main/assets/logger/logger.js
@@ -1,88 +1,44 @@
 define(['module'], function (module) {
   'use strict';
 
+  var noop = function () {
+  };
+
   var config = (module.config && module.config()) || {};
-  var onDebug = config.onDebug || function () {
-    };
-  var onInfo = config.onInfo || function () {
-    };
-  var onWarn = config.onWarn || function () {
-    };
-  var onError = config.onError || function () {
-    };
-  var onFatal = config.onFatal || function () {
-    };
-
-  return function (self, name, isDebug) {
-    var prefix = '[' + name + ']';
-    var log = function (onLog, fatal) {
-      return function () {
-        var args = [prefix];
-        for (var i = 0, l = arguments.length; i < l; i++) {
-          args.push(arguments[i]);
-        }
-        if (fatal) {
-          onLog(args);
-          throw new Error(args);
-        } else
-          return onLog(args);
-      };
-    };
-
-    var cons = console;
-    var debug;
-    if (isDebug) {
-      debug = log(function (args) {
-        try {
-          onDebug.apply(self, args);
-        } catch(exception) {
-          cons.warn.apply(self, [prefix, 'onDebug hook failed', exception]);
-        }
-        return cons.debug.apply(self, args);
-      });
-    } else {
-      debug = function () {
-      };
+  var levels = {
+    'log': 5,
+    'debug': 4,
+    'info': 3,
+    'warn': 2,
+    'error': 1,
+    'off': 0
+  };
+  var defaultLevel = levels.hasOwnProperty(config.defaultLevel) && config.defaultLevel || 'log';
+  var defaultPriority = levels[defaultLevel];
+  var callbacks = config.callbacks || {};
+  for (var level in levels) {
+    if (levels.hasOwnProperty(level) && !callbacks[level]) {
+      callbacks[level] = noop;
     }
-    var info = log(function (args) {
-      try {
-        onInfo.apply(self, args);
-      } catch(exception) {
-        cons.warn.apply(self, [prefix, 'onInfo hook failed', exception]);
-      }
-      return cons.info.apply(self, args);
-    });
-    var warn = log(function (args) {
-      try {
-        onWarn.apply(self, args);
-      } catch(exception) {
-        cons.warn.apply(self, [prefix, 'onWarn hook failed', exception]);
-      }
-      return cons.warn.apply(self, args);
-    });
-    var error = log(function (args) {
-      try {
-        onError.apply(self, args);
-      } catch(exception) {
-        cons.warn.apply(self, [prefix, 'onError hook failed', exception]);
-      }
-      return cons.error.apply(self, args);
-    });
-    var fatal = log(function (args) {
-      try {
-        onFatal.apply(self, args);
-      } catch(exception) {
-        cons.warn.apply(self, [prefix, 'onFatal hook failed', exception]);
-      }
-      return cons.error.apply(self, args);
-    }, true);
+  }
 
-    return {
-      debug: debug,
-      info: info,
-      warn: warn,
-      error: error,
-      fatal: fatal
-    };
+  return function (self, name, loggerLevel) {
+    var logger = {};
+    logger.name = name;
+    logger.level = levels.hasOwnProperty(loggerLevel) && loggerLevel || defaultLevel;
+    logger.priority = Math.min(levels[logger.level], defaultPriority);
+
+    var prefix = '[' + name + ']';
+    var con = window.console;
+    for (var level in levels) {
+      if (levels.hasOwnProperty(level)) {
+        if (con && con[level] && levels[level] <= logger.priority)
+          logger[level] = con[level].bind(con, prefix);
+        else
+          logger[level] = noop;
+      }
+    }
+
+    return logger;
   };
 });
--- src/main/assets/util/util.js
+++ src/main/assets/util/util.js
@@ -1,16 +1,22 @@
 define(['logger'], function (logger) {
   "use strict";
-
   return function (self, module) {
     var config = (module.config && module.config()) || {};
-    var logger = logger(self, module.id, config.debug);
 
-    debug('configuration: ', config);
+    var base = '';
+    var separatorIndex = module.id.lastIndexOf('/');
+    if (separatorIndex >= 0)
+      base = module.id.substring(0, separatorIndex);
+
+    var _logger = logger(self, module.id, config.logger || config.debug && 'debug' || 'info');
+
+    _logger.debug('configuration: ', base, config);
 
     return {
       module: module,
       config: config,
-      logger: logger
+      base: base,
+      logger: _logger
     };
   };
 });
